8a68f35a34c714ca4d0fdd0f02f6104a7ab230c9
Bruno <bruno.sant.a@gmail.com>
2017-11-25 00:13:16 -0300

    Descricao metodo wazeScraping

--- baltar_met.py
+++ baltar_met.py
@@ -10,6 +10,19 @@ class data:
     def __init__(self):
         pass
     def wazeScraping(self, latitude, longitude):
+        ''' Recupera o nível de congestionamento próximo ao local selecionado.
+
+            Argumentos
+            ---------
+
+            latitude: Latitude do local a ser analizado 
+            longitude: Latitude do local a ser analizado
+
+            Retorna
+            -------
+            severity: Número de pontos congestionados por nível de congestionamento [1..4]
+
+        '''
         driver = webdriver.Chrome()
         ac = ActionChains(driver)
         url = 'https://www.waze.com/pt-BR/livemap?zoom=17&lat={0:.6f}&lon={1:.6f}'.format(latitude, longitude)
